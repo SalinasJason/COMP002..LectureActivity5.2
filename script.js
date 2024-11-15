4aa7568ed31047a6249501656286573c482ae186
SalinasJason <jasonsalinas770@gmail.com>
2024-11-14 18:26:02 -0800

    added link with event listener that prevents

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
         button.removeEventListener("click", buttonClick);
     }
     button.addEventListener("click", buttonClick);
+
+   
+
+    let link = document.querySelector("#link1");
+
+    function LinkClick(event) {
+        event.preventDefault(); 
+        alert("No distractions! I'm coding!");
+    }
+    link.addEventListener("click", LinkClick);
 });
